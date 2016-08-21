1a4c4d051146db45f3067fcfb15e042d8641a7b3
Jason Gorman <jason.gorman@codemanship.com>
2016-08-21 22:44:52 +0100

    Another tweak, standardising to assertThat in tests

--- SilverBarsMarketplace/test/com/silverbars/liveorders/LiveOrderBoardTests.java
+++ SilverBarsMarketplace/test/com/silverbars/liveorders/LiveOrderBoardTests.java
@@ -1,10 +1,8 @@
 package com.silverbars.liveorders;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import java.util.Arrays;
@@ -31,7 +29,7 @@ public class LiveOrderBoardTests {
 	public void registeringOrderAddsItToOrderBoard() {
 		Order order = mock(Order.class);
 		registerOrder(order);
-		assertTrue(orderBoard.contains(order));
+		assertThat(orderBoard.contains(order), is(true));
 	}
 
 	@Test
@@ -39,7 +37,7 @@ public class LiveOrderBoardTests {
 		Order order = mock(Order.class);
 		registerOrder(order);
 		orderBoard.cancelOrder(order);
-		assertFalse(orderBoard.contains(order));
+		assertThat(orderBoard.contains(order), is(false));
 	}
 
 	@Test
@@ -49,7 +47,7 @@ public class LiveOrderBoardTests {
 			registerOrder(order);
 		}
 		List<OrderSummaryItem> summaries = orderBoard.summariseOrdersByType(OrderType.BUY);
-		assertThat(summaries.get(0).getTotalQuantity(), is(totalQuantity));
+		assertThat(summaries.get(0).getTotalQuantity(), equalTo(totalQuantity));
 	}
 
 	@Test
@@ -60,8 +58,8 @@ public class LiveOrderBoardTests {
 		registerOrder(createOrder(type, 100, 1.0));
 		registerOrder(createOrder(type, 200, 1.0));
 		List<OrderSummaryItem> summaries = orderBoard.summariseOrdersByType(type);
-		assertEquals(firstPrice, summaries.get(0).getPrice(), 0);
-		assertEquals(secondPrice, summaries.get(1).getPrice(), 0);
+		assertThat(summaries.get(0).getPrice(), equalTo(firstPrice));
+		assertThat(summaries.get(1).getPrice(), equalTo(secondPrice));
 	}	
 
 	private Object[] orders(){
